68c65aa097303c773bb84e72ea01c638e174ceb2
MarcinMichalak995 <marcin.michalak995@gmail.com>
2023-01-25 20:04:19 +0100

    001

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-#id01{
-    color: blue;
+function myFunction() {
+
+    const element = document.getElementById("id01");
+
+    element.innerHTML = "New Heading";
+
+}
+
+​
+
+function myColorChange() {
+
+    const element = document.getElementById("id01");
+
+    element.style.color = "red";
+}
+
+    function mojaFunkcja(){
+
 }
