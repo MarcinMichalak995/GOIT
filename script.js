493bb25d5f2debdf149565bf5a6fad1d6b90b2eb
MarcinMichalak995 <marcin.michalak995@gmail.com>
2023-02-06 16:10:16 +0100

    Zajecia5

--- script.js
+++ script.js
@@ -52,20 +52,74 @@ function mojaFunkcja(id) {
   }
 
 }
-
-
-     
+function dodawanie (a, b, c) {
+  return a + b + c;
   
-      
-  
+}
 
+
+
+
+//function greet(name2) {
+  //console.log(`Pozdrawiam ${name2}.`);
+  //return name2;
+//}
+
+//function registerGuest(name, parametr) {
+  //console.log(`Rejesttracja gościa ${name}.`);
+  //parametr(name);
+//}
+//greet("Kiwi");
+//console.log(greet("Kiwi"));
+//console.log(greet);
+
+//registerGuest("Mango", greet);
       
-     
-      
-      
-      
-      
+//function processCall(recipient) {
+
+  //const liczba = Math.random()>0.5;
+
+  //console.log(liczba);
+  //console.log(!liczba);
+
+  //if (!liczba) {
+    //console.log(`Abonent ${recipient} jest niedostęny, zostaw wiadomość`);
+
+  //} else {
+    //console.log(`Łączenie z ${recipient}, proszę czekać...`);
+  //}
+//}      
     
-      
+ //processCall("Mango");  
+ 
+ function processCall(recipient, onAvailable, onNotAvailable) {
+  // Symuluj dostępność abonenta za pomocą liczby losowej
+    const isRecipientAvailable = Math.random() > 0.5;
+  
+    if (!isRecipientAvailable) {
+      onNotAvailable(recipient);
+      return;
+    }
+  
+    onAvailable(recipient);
+  }
+  
+  function takeCall(name) {
+    console.log(`Łączenie z ${name}, proszę czekać...`);
+  // Logika odbierania połączenia
+  }
+  
+  function activateAnsweringMachine(name) {
+    console.log(`Abonent ${name} jest niedostępny, zostaw wiadomość.`);
+  // Logika aktywacji automatycznej sekretarki
+  }
+  
+  function leaveHoloMessage(name) {
+    console.log(`Abonent ${name} jest niedostępny, nagrywamy hologram.`);
+  // Logika nagrywania hologramu
+  }
+  
+  processCall("Mango", takeCall, activateAnsweringMachine);
+  processCall("Poly", takeCall, leaveHoloMessage);
       
   
